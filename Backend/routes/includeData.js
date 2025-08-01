2c8606a3bc5ce51860ece1dfac3fed6acd08a6a4
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2025-08-01 19:47:33 -0400

    added env

--- Backend/routes/includeData.js
+++ Backend/routes/includeData.js
@@ -1,6 +1,6 @@
 const express = require('express'); 
 const includedata = express.Router();  
-const mysql = require('mysql'); 
+
 
 includedata.get('/getuserData', (req, res) => { 
     res.send('This route gets the userdata its where the database is going to be setup'); 

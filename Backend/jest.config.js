f1928daec976888063388320260c01bf68bcef14
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2025-08-01 17:16:28 -0400

    added this file

--- Backend/jest.config.js
+++ Backend/jest.config.js
@@ -1,8 +1,10 @@
-// jest configure file to help with test coverage for the project.  
+// This file is for if i want to add the jest coverage to see if code is covered by tests etc.  
+// commenting out this module exports so that it just runs as npm t without the coverage for now.  so just regular jest without the test coverage chart showing. 
 
-module.exports = { 
-    collectCoverage: true, 
-    collectCoverageFrom: ["tests/*/*.js"], 
-    coverageDirectory: "coverage", 
-    testMatch: ["**/tests/**/*.test.js"], 
-}; 
+// module.exports = { 
+//     collectCoverage: true, 
+//     collectCoverageFrom: ["name of where the file is that you want to test or routes etc/**/*.js"],
+//     coverageDirectory: "coverage", 
+//     testMatch: ["**/tests/**/*.test.js"], 
+// }; 
+
--- Backend/tests/random.test.js
+++ Backend/tests/random.test.js
@@ -1,13 +1,13 @@
-// just making sure if jest is correctly installed on the project 
-describe('testing these two lines of tests', () => { 
-    test('this', () => {
-        expect(2+2).toBe(4); 
-    }); 
 
-    test('this one as well', () => { 
-        const data = {"name": 'Ray'}; 
-        data['lname'] = 'TheDon'; 
-        expect(data).toEqual({"name": 'Ray', 'lname': 'TheDon'}); // remember with objects to see if they are equal with what you want to expect remember to use toEqual matcher
-    }); 
 
-})
+// just making sure if jest is correctly installed on the project  
+// testing if the coverage will take care of the files as such if they are only imported remember im using CommonJS 
+const testing = require('../routes/function.js'); 
+ 
+
+describe('Testing whether the coverage file will work based of imported files', () => { 
+    test('whether it will recognize files and lines etc', () => { 
+        expect(testing(2, 4)).toBe(6); 
+    }); 
+}); 
+

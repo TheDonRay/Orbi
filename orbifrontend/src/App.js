a5ca44c390fb4a9abd77bad07d7bb89a419ef2f9
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2025-08-07 11:48:55 -0400

    Routes added

--- orbifrontend/src/App.js
+++ orbifrontend/src/App.js
@@ -1,15 +1,19 @@
 import React from 'react'; 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
+
 
 // import the pages stuff here 
 const HomePage = require('./pages/homepage.jsx'); 
 const AboutPage = require('./pages/aboutpage.jsx'); 
 
 function App() {
-  return ( 
-    <div> 
-     <HomePage />  
-     <AboutPage />
-    </div>
+  return (  
+    <Router>
+      <Routes> 
+        <Route exact path = "/home" element={<HomePage />} /> 
+        <Route exact path = "/about" element={<AboutPage />} />
+      </Routes>
+    </Router>
   )
 }
 

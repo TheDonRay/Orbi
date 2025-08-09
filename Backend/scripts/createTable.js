bbba7f91e21cfd3669d8967e29b28bdba029702f
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2025-08-08 23:26:33 -0400

    error fixed

---
+++ Backend/scripts/createTable.js
@@ -0,0 +1,15 @@
+// const dbConnection = require('../Database/db.js'); 
+
+// // create a table as such 
+// const createTableQuery = `  
+//     CREATE TABLE IF NOT EXISTS users ( 
+//         id INTEGER PRIMARY KEY, 
+//         Tasks TEXT NOT NULL 
+//         email TEXT NOT NULL 
+//     )
+// `; // this is an example query to create a table  
+
+// // now we need to connect to the SQllite database and execute the create table query 
+// dbConnection.serialize(() => [ 
+//     dbConnection.listenerCount(c)
+// ])

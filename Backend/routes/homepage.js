7426439872289eb397b2eeefa1ff04ed88eb7344
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2025-08-12 17:17:24 -0400

    working on the data in the backend to show up

--- Backend/routes/homepage.js
+++ Backend/routes/homepage.js
@@ -5,7 +5,8 @@ const homepage = express.Router();
 homepage.get('/', (req, res) => { // note another thing u need to realize is that you cant have two res.json u need to wrap it into one. 
     res.json({
         btntitle: "Get Started",     
-        secondbtntitle: "Learn More"
+        secondbtntitle: "Learn More", 
+        cardData: "Orbi wasn't just an idea I thought of from the rip of my mind it was something I found annoying every morning that I woke up to either go to work, school, or even my day to day life."
     });   
 }); 
 

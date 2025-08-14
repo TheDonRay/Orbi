f63d5dba5a7502d2cbf62c40a946227532d916c3
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2025-08-13 20:51:07 -0400

    should be sql

--- Backend/Database/CreateTables.js
+++ Backend/Database/CreateTables.js
@@ -1,45 +1,30 @@
 const dbConnection = require('./dbconnection'); 
 
 // create a table as such 
-const createUserTable = `  
+const createTables = `  
     CREATE TABLE IF NOT EXISTS users ( 
-        id INTEGER PRIMARY KEY AUTOINCREMENT, 
-        email TEXT UNIQUE NOT NULL
+        id INTEGER PRIMARY KEY AUTO_INCREMENT, 
+        email VARCHAR(255) UNIQUE NOT NULL
     ); 
-`;  
-
-// creating another datatable here as such 
-const createTasksTable = ` 
-    CREATE TABLE IF NOT EXISTS tasks (      
-        id INTEGER PRIMARY KEY AUTOINCREMENT,  
-        tasks TEXT UNIQUE NOT NULL 
-    ); 
+    
+    CREATE TABLE IF NOT EXISTS tasks ( 
+        id INTEGER PRIMARY KEY AUTO_INCREMENT, 
+        user_id INTEGER NOT NULL,
+        task_text VARCHAR(255) NOT NULL,
+        FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+    );
 `; 
 // now we need to connect to the SQllite database and execute the create table query 
 function createTable() {  
-        try { // i implement the outside try and catch to catch any synchronous errors compared to the inside which catch callback errors to the serialize function. 
-            dbConnection.serialize(() => {
-                dbConnection.run(createUserTable, (err) => {
-                    if (err) {
-                        console.error('Error creating table:', err.message);
-                    } else {
-                        console.log("Table was created successfully");
-                    }
-                });
-
-
-                dbConnection.run(createTasksTable, (err) => { // notice both of these are under the  one serialize call back function 
-                    if (err) {
-                        console.error('Error creating tasks tabke', err.message); 
-                    } 
-                    else { 
-                        console.log('Table was created Successfully'); 
-                    }
-                }); 
-            });  
-    } catch (err) { 
-        console.error('Error running database create connection', err.message); 
-    }
+      // instead we should follow mysql NOT SQLITE what i was doing before 
+      // instead we use pool.execute 
+      pool.execute(createTables, (err, results) => { 
+        if (err) { 
+            console.error('Error creating tables', err.message); 
+        } else { 
+            console.log('Tables Created Successfully', results); 
+        }
+      })
 } 
 
 module.exports = createTable; // exported this because we need to call this create table upon server running. 

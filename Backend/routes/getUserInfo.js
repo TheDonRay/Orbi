3538d32fd5325b3f0dcf72c973fd1a0601bb09ef
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2025-08-16 16:57:42 -0400

    working on the crud operation for the sql database

--- Backend/routes/getUserInfo.js
+++ Backend/routes/getUserInfo.js
@@ -1,16 +1,21 @@
 const express = require('express'); 
 const getdata = express.Router();   
-
+// import the connection here 
+const connection = require('../Database/dbconnection'); 
 
 // route to get the user data below going to test out from client side 
-getdata.post('/userdata', async (req, res) => {  
-    try { 
-        const { id, email } = req.body;  
-        // TODO implement here 
-        // TODO figure out how to implement the post data. 
-    } catch(err) { 
+getdata.post('/userdata', (req, res) => {  
+    // start destructuring as such 
+    const { id, email } = req.body; 
+    const query = 'INSERT INTO users (name, email) VALUES (?, ?)'; 
 
-    }
+    connection.query(query, [id, email], (err, results) => { 
+        if (err){ 
+            console.error(err); 
+            return res.status(500).json({message: 'Database Error', error: err }); 
+        } 
+        res.status(201).json({ message: 'User added', userId: results.insertId }); 
+    })
 }); 
 
 module.exports = getdata;  

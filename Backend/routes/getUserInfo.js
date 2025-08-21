334adbbd40f1e73bcc8b66d787d542ffa7b760c9
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2025-08-20 20:55:25 -0400

    fixed this issue

--- Backend/routes/getUserInfo.js
+++ Backend/routes/getUserInfo.js
@@ -7,25 +7,38 @@ const connection = require('../Database/dbconnection');
 
 
 // route to create new user
-getdata.post('/userdata', (req, res) => {
+getdata.post('/userdata', async (req, res) => {
     // implement the old way where i start of with traditional callback here 
     const { name, email } = req.body;   
 
     // want to do a check if there is nothing in the body 
-    if (!req.body){ 
+    if (!name || !email){ 
         return res.status(404).send('Error nothing recieved in the body request'); 
     }
-    const UserQuery = 'INSERT INTO users (name, email) VALUES (?, ?)'; 
-    connection.query(UserQuery, [name, email], (err, result) => { 
-        // start of with our error handling 
-        if (err) { 
-            console.error(err); 
-        } 
-        console.log('Successfully inserted new user to the Database'); 
+    const UserQuery = 'INSERT INTO users (name, email) VALUES (?, ?)';  
+    
+    // since im using a connection pool method that returns a promise, not a callback 
+    try { 
+        const [result] = await connection.query(UserQuery, [name, email]); 
+        console.log('Successfully inserted into the DataBase');  
         res.status(201).send({ id: result.insertId, name, email }); 
-    }); 
+    } catch (error) { 
+        console.error('Database Error', err); 
+        res.status(500).send('Database Error'); 
+    }
 });
 
 
-// create another endpoint here just to test out your understanding. 
+// create another endpoint here to get the data from the sql database 
+// getdata.get('/fetchdata', (req, res) => { 
+//     // start the destructuring  
+//     const { name, email } = req.body;    
+
+//     // first check for null values 
+//     if ()
+//     const fetchQuery = 'SELECT * FROM users'; 
+
+
+// })
+
 module.exports = getdata;

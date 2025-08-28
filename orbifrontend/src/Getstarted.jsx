95c13d55d2ee752f03f58904bb3de8d06f7dd015
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2025-08-27 22:16:41 -0400

    added the backend data part

--- orbifrontend/src/Getstarted.jsx
+++ orbifrontend/src/Getstarted.jsx
@@ -15,15 +15,36 @@ function Getstarted() {
     to your handler. By convention, we call it e (short for “event”), 
     though you could name it anything*/
     // form submission handler 
-    function onSubmitfromUser(e) { // the e here stands for even tobject that the broswer passes to event handlers when something happens like a form submission a click or key press
+    async function onSubmitfromUser(e) { // the e here stands for even tobject that the broswer passes to event handlers when something happens like a form submission a click or key press
         
         e.preventDefault();  //prevents the page from reloading. 
 
         console.log('Name Entered: ', name); 
         console.log('Email Entered', email);   
 
-        //TODO send that data to backend to the post route 
+        // we can first go ahead and destructure the values as such 
+        const userInfo = { name, email}; 
 
+        //set up a try and catch case 
+        try { // error that i faced was i didnt pass the entire backend route here to that specific endpoint
+            const response = await fetch('http://localhost:8000/api/v1/user/userdata', { 
+                method: 'POST', 
+                headers: { 
+                    'Content-Type' : 'application/json', 
+                }, 
+                body: JSON.stringify(userInfo), 
+            });  
+            
+            if (response.ok) { 
+                const result = await response.json(); 
+                console.log('User added their name and email Successfully:', result); 
+            } 
+            else { 
+                console.error('User added information failed', response.statusText); 
+            }
+        } catch (error) { 
+            console.error('Error getting userinfo to backend', error);
+        }
 
         // in the end reset the form 
         setName(""); 
